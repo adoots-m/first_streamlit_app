9a1ecc53f74e9596b670bb93956b450d02920584
Adithya Menon <71412758+adoots-m@users.noreply.github.com>
2023-06-15 20:20:26 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit
 import pandas
-
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
 streamlit.title("New test file")
 
 streamlit.header('🥣  Breakfast Menu')
