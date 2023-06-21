cabfc95cc71d11b071a7fd540e5de7213e21442a
Adithya Menon <71412758+adoots-m@users.noreply.github.com>
2023-06-21 21:20:21 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,16 +44,42 @@ try:
 except URLError as e:
   streamlit.error()
   
+def get_fruit_load_list():
+  with conn.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list;")
+    conn.execute("select * from fruit_load_list;")
+    return my_cur.fetchall()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])  
-my_cur = my_cnx.cursor()
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains")
-streamlit.dataframe(my_data_rows)
+@streamlit.experimental_singleton
+def init_connection():
+    return psycopg2.connect(**streamlit.secrets["postgres"])
 
-fruit_add = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding', fruit_add)
+  
+streamlit.header("View Our Fruit List - Add Your Favorites!")
+# Add a button to load the fruit
+if streamlit.button('Get Fruit List'):
+  conn = init_connection()
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  conn.close()
+  my_cnx.close()
+  streamlit.dataframe(my_data_rows)
+
+
+def insert_row_snowflake(new_fruit):
+  with conn.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list (fruit_name) values ('" + new_fruit + "');")
+    return "Thanks for adding " + new_fruit
+
+  
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  conn = init_connection()
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  conn.commit()
+  conn.close()
+  streamlit.text(back_from_function)
 
 
 
